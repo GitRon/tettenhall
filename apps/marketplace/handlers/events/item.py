768a5ca86ce34eeba5f21d9b9f025a1a125efb4c
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2023-01-22 19:32:25 +0100

    Add sold items to marketplace

---
+++ apps/marketplace/handlers/events/item.py
@@ -0,0 +1,9 @@
+from apps.core.domain import message_registry
+from apps.item.messages.events import item
+from apps.marketplace.models.marketplace import Marketplace
+
+
+@message_registry.register_event(event=item.ItemSold)
+def handle_add_sold_item_to_marketplace(context: item.ItemSold.Context):
+    marketplace = Marketplace.objects.all().first()
+    marketplace.available_items.add(context.item)
