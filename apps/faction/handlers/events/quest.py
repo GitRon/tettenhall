a35fcdd7070260cc918af9783d23e135946bfc3b
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-03-07 13:04:27 +0100

    Added todo

--- apps/faction/handlers/events/quest.py
+++ apps/faction/handlers/events/quest.py
@@ -5,5 +5,6 @@ from apps.quest.messages.events.quest import QuestAccepted
 
 @message_registry.register_event(event=QuestAccepted)
 def handle_set_active_quest_on_acceptance(*, context: QuestAccepted):
+    # TODO: how can i lint that events don't change anything in the database?
     faction = context.accepting_faction
     faction.active_quests.add(context.quest_contract)
